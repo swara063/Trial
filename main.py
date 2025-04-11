0ff11f12fd963186f95948a7b8179100620b1d04
Swara Sameer Pawanekar <156062610+swara063@users.noreply.github.com>
2025-04-11 14:24:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def get_token_from_url():
         authorization_code = st.experimental_get_query_params()['code'][0]
         client_id = 155063  # Replace with your Strava app client ID
         client_secret = b37c50a0d719153af7cea2811c3405d4070d8616  # Replace with your Strava app client secret
-        redirect_uri = 4a24c768b52facd939c63db33fc6218321a8f9c0  # Replace with the redirect URI you set in Strava
+        #redirect_uri = 4a24c768b52facd939c63db33fc6218321a8f9c0  # Replace with the redirect URI you set in Strava
         
         # Prepare the payload for token exchange
         token_url = 'https://www.strava.com/api/v3/oauth/token'
